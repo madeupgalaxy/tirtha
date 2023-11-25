d044d16e28f00a7c7982c9cca86e02e3b249d47b
Rahil <118007944+madeupgalaxy@users.noreply.github.com>
2023-11-25 23:55:10 +0530

    Create welcome.dart

---
+++ lib/pages/welcome.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Welcomepage extends StatefulWidget {
+  const Welcomepage({super.key});
+
+  @override
+  State<Welcomepage> createState() => _WelcomepageState();
+}
+
+class _WelcomepageState extends State<Welcomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(itemBuilder: (_, index) {}),
+    );
+  }
+}
